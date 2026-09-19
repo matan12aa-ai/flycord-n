4492af78ac90bb63ecb83437d93957a1ae9e2179
matan12aa-ai <matan12aa@gmail.com>
2026-09-19 18:33:14 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -2038,4 +2038,6 @@ if __name__ == "__main__":
     else:
         # make sure tables exist even if the db file was created empty
         init_db()
-    app.run(host="0.0.0.0", port=5000, debug=True)
+
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
